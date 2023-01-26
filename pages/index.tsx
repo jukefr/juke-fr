cb48641c2d2053aa33f334572bd6f7e39e3c8f3b
Kay <git@juke.fr>
2023-01-26 05:08:56 +0100

    revert back to server side whatever

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,30 @@
 import { Flex, Heading } from '@chakra-ui/react';
 import Projects from '../components/Projects';
-import { useState, useEffect } from 'react';
 
 interface IHomeProps {
   projects: any;
 }
 
-const HomePage = (): JSX.Element => {
-  const [projects, setProjects] = useState(null);
-  const [isLoading, setLoading] = useState(false);
+export async function getStaticProps() {
+  const latestURL = new URL('https://code.juke.fr/api/v1/users/kay/repos');
+  latestURL.search = new URLSearchParams().toString();
+  const projects = await (await fetch(latestURL.toString()))
+    .json()
+    .then((res) =>
+      res.sort((a: any, b: any) => {
+        return Date.parse(b.updated_at) - Date.parse(a.updated_at);
+      }),
+    );
 
-  useEffect(() => {
-    setLoading(true);
-    fetch('https://code.juke.fr/api/v1/users/kay/repos')
-      .then((res) => res.json())
-      .then((res) =>
-        res.sort((a: any, b: any) => {
-          return Date.parse(b.updated_at) - Date.parse(a.updated_at);
-        }),
-      )
-      .then((projects) => {
-        setProjects(projects);
-        setLoading(false);
-      });
-  }, []);
-  if (isLoading) return <p>loading...</p>;
-  if (!projects) return <p>error fetching projects :O</p>;
+  return {
+    props: {
+      projects,
+    },
+    revalidate: 60 * 60,
+  };
+}
+
+const HomePage = ({ projects }: IHomeProps): JSX.Element => {
   return (
     <Flex alignItems="center" justifyContent="center" direction="column">
       <Flex direction="column" rounded={6} mb={12}>
