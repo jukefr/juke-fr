923414eb88b969f2d37d13e3c8daf40753bee9ed
Kay <git@juke.fr>
2023-08-27 17:56:58 +0200

    h

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,22 +7,26 @@ import {
   Flex,
   Text,
 } from '@chakra-ui/react';
+import { A11yUserPreferences } from '@react-three/a11y';
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
+import { Clock } from 'three';
 import Navbar from '../components/Navbar';
+import theme from '../components/assets/chakraTheme';
+import getRandomShader from '../components/assets/shaders';
+import '../components/global.css';
 import Jinx from '../components/jinx/Jinx';
 import {
   store as defaultStore,
   getFromLocalStorage,
   writeToLocalStorage,
 } from './store';
-import { Clock } from 'three';
-import getRandomShader from '../components/assets/shaders';
-import { A11yUserPreferences } from '@react-three/a11y';
-import theme from '../components/assets/chakraTheme';
 
-import '../components/global.css';
+// export const metadata: Metadata = {
+//   title: 'My Page Title',
+// };
 
-export default function DashboardLayout({
+export default function Layout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
@@ -90,6 +94,66 @@ export default function DashboardLayout({
   return (
     <html>
       <body>
+        {/* <Head>
+          <title>juke.fr</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="creates mostly ephemeral, always open source pet projects, sometimes."
+            key="meta-description"
+          />
+          <meta
+            name="keywords"
+            content="Javascript, Node, Typescript, React, Next.js, Docker"
+            key="meta-keywords"
+          />
+          <meta name="author" content="Kay" key="meta-author" />
+
+          <meta property="og:title" content="juke.fr" key="meta-og-title" />
+          <meta
+            property="og:description"
+            content="creates mostly ephemeral, always open source pet projects, sometimes."
+            key="meta-og-description"
+          />
+          <meta
+            property="og:image"
+            content="https://juke.fr/og.png"
+            key="meta-og-image"
+          />
+          <meta property="og:type" content="website" key="meta-og-type" />
+          <meta
+            property="og:site_name"
+            content="juke.fr"
+            key="meta-og-site_name"
+          />
+          <meta property="og:locale" content="en_US" key="meta-og-locale" />
+
+          <meta name="twitter:card" content="summary" key="meta-twitter-card" />
+          <meta name="twitter:site" content="@jukefr" key="meta-twitter-site" />
+          <meta
+            name="twitter:creator"
+            content="@jukefr"
+            key="meta-twitter-creator"
+          />
+          <meta
+            name="twitter:title"
+            content="juke.fr"
+            key="meta-twitter-title"
+          />
+          <meta
+            name="twitter:description"
+            content="creates mostly ephemeral, always open source pet projects, sometimes."
+            key="meta-twitter-description"
+          />
+          <meta
+            name="twitter:image"
+            content="https://juke.fr/og.png"
+            key="meta-twitter-image"
+          />
+        </Head> */}
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <ChakraProvider theme={theme}>
           <A11yUserPreferences>
