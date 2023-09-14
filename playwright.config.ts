4d2578e1308d63ce082f193eec3377ad09718823
Kay <contact@juke.fr>
2023-09-14 19:40:00 +0200

    change script

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ const config: PlaywrightTestConfig = {
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
     command: 'npm run start',
-    port: 3333,
+    port: 3000,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
