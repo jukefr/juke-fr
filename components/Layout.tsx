1e8f15801045403cd36e7cbd5c766a5c41566091
Kate <kate@juke.fr>
2024-08-06 02:52:44 +0200

    no overflow x

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,14 +5,14 @@ import Navbar from './Navbar';
 import JinxWrapper from './jinx/JinxWrapper';
 
 export default function Layout({ fronter }: { fronter: string }) {
-  return (
-    <A11yUserPreferences>
-      <div className="px-4 md:container md:mx-auto">
-        <Navbar />
-      </div>
-      <main>
-        <JinxWrapper fronter={fronter} />
-      </main>
-    </A11yUserPreferences>
-  );
+	return (
+		<A11yUserPreferences>
+			<div className="px-4 md:container md:mx-auto">
+				<Navbar />
+			</div>
+			<main className='overflow-x-hidden'>
+				<JinxWrapper fronter={fronter} />
+			</main>
+		</A11yUserPreferences>
+	);
 }
