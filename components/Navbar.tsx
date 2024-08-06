b2cc2ede375dbe2725e6083979dc5a9fbb7e4215
Kate <kate@juke.fr>
2024-08-06 02:37:58 +0200

    better mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,53 +8,57 @@ import ToggleJinx from './togglers/ToggleJinx';
 import ToggleReducedMotion from './togglers/ToggleReducedMotion';
 
 const NavbarButtons = () => {
-  const [showJinx, setShowJinx] = useState('false');
-  useEffect(() => {
-    // ! handle localStorage changes from
-    // ! - toggleJinx
-    window.addEventListener('storage', () => {
-      setShowJinx(localStorage.showJinx);
-    });
-    setShowJinx(localStorage.showJinx);
-    return window.removeEventListener('storage', () => {});
-  }, []);
+	const [showJinx, setShowJinx] = useState('false');
+	useEffect(() => {
+		// ! handle localStorage changes from
+		// ! - toggleJinx
+		window.addEventListener('storage', () => {
+			setShowJinx(localStorage.showJinx);
+		});
+		setShowJinx(localStorage.showJinx);
+		return window.removeEventListener('storage', () => { });
+	}, []);
 
-  return (
-    <div className="contents">
-      {showJinx === 'true' && (
-        <div className="contents">
-          <div>
-            <ToggleEditor />
-          </div>
-          <div>
-            <ToggleReducedMotion />
-          </div>
-        </div>
-      )}
-      <div>
-        <ToggleJinx />
-      </div>
-      <div>
-        <ToggleColorMode />
-      </div>
-    </div>
-  );
+	return (
+		<div className="contents">
+			{showJinx === 'true' && (
+				<div className="contents">
+					<div>
+						<ToggleEditor />
+					</div>
+					<div>
+						<ToggleReducedMotion />
+					</div>
+				</div>
+			)}
+			<div>
+				<ToggleJinx />
+			</div>
+			<div>
+				<ToggleColorMode />
+			</div>
+		</div>
+	);
 };
 
-const Navbar = ({}) => {
-  return (
-    <nav>
-      <div className="flex mt-6 mb-12 align-bottom flex-wrap">
-        <NavLink href="/">
-          <h1>{'juke'}</h1>
-        </NavLink>
-        <NavLink href="/boops">boops</NavLink>
-        <NavLink href="/about">about</NavLink>
-        <div className="grow" />
-        <NavbarButtons />
-      </div>
-    </nav>
-  );
+const Navbar = ({ }) => {
+	return (
+		<nav>
+			<div className="flex flex-wrap justify-between items-center mb-4 mt-4">
+				<div className="flex flex-col md:flex-row md:items-center">
+					<NavLink href="/">
+						<h1>{'juke'}</h1>
+					</NavLink>
+					<NavLink href="/boops">boops</NavLink>
+					<NavLink href="/about">about</NavLink>
+				</div>
+				<div className="grow" />
+				<div className="flex flex-wrap w-full md:w-auto justify-end">
+					<NavbarButtons />
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
